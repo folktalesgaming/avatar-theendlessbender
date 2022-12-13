723ad93503376538fac758f5b4a2e7035f9fdbef
alsongarbuja <garbuja12alson@gmail.com>
2022-12-13 15:51:38 +0545

    prefab: enemy power prefab and controller script

---
+++ Assets/Scripts/EnemyPowerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPowerController : MonoBehaviour
+{
+    private float speed_f = 20.0f;
+    private Rigidbody2D power;
+    private PlayerController playerController;
+
+    void Start()
+    {
+        power = GetComponent<Rigidbody2D>();
+        power.velocity = transform.right * speed_f;
+    }
+
+    void Update()
+    {
+        if(transform.position.x > 15 || transform.position.x < -15) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")) {
+            playerController = other.gameObject.GetComponent<PlayerController>();
+            playerController.TakeDamage(10);
+            Destroy(gameObject);
+        }
+    }
+}
