1e031cb75e853d29b0c1e204707464fb6141374f
alsongarbuja <garbuja12alson@gmail.com>
2022-12-21 13:49:30 +0545

    rename: rename tocuhcontroller to touchcontroller

--- Assets/Scripts/TouchController.cs
+++ Assets/Scripts/TouchController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TocuhController : MonoBehaviour
+public class TouchController : MonoBehaviour
 {
     private PlayerController playerController;
 
