059d61eac5f9cb3b7acb4dce064e35e961282cc6
alsongarbuja <garbuja12alson@gmail.com>
2022-12-31 13:32:15 +0545

    add: add a exit script to exit from main menu screen

--- Assets/Scripts/SystemManager.cs
+++ Assets/Scripts/SystemManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SystemManager: MonoBehaviour {
-    void update() {
-        if (Input.GetKeyDown(KeyCode.Escape)) 
-            Application.Quit(); 
+    public void Exit() { 
+        Application.Quit(); 
     }
 } 
