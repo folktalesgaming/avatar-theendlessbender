2d9b20bdb5f4ceb176f67e3eace154b87ed41345
alsongarbuja <garbuja12alson@gmail.com>
2023-04-16 14:50:24 +0545

    audio: add audio and exit pop up script

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -5,7 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+
+    private AudioSource audioSource;
+    [SerializeField] GameObject exitPopup;
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void StartGame() {
+        audioSource.Play();
         SceneManager.LoadScene("FirstLevel");
+    }
+
+    public void OpenExitPopUp() {
+        audioSource.Play();
+        exitPopup.SetActive(true);
+    }
+
+    public void CloseExitPopUp() {
+        audioSource.Play();
+        exitPopup.SetActive(false);
+    }
+
+    public void Exit() { 
+        Application.Quit(); 
     }
 }
--- Assets/Scripts/SystemManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SystemManager: MonoBehaviour {
-    public void Exit() { 
-        Application.Quit(); 
-    }
-} 
